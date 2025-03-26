906b8fdbd44737a64e87be6be51cdc2ce56dde30
lsy <lsy971021@gmail.com>
2025-03-26 20:21:01 +1030

    problem solving

---
+++ practical/WK4/menu.py
@@ -0,0 +1,26 @@
+# This is my first Python code.
+# liusy088
+#
+
+print("Welcome to Mawson Lakes Deli!")
+name = 'Siyuan Liu'
+name = input("What is your name?")
+print('Hello!', name, '!')
+
+print()
+print("=========Drinks=========")
+print("Cock, Sprite, Lift, Orange Juice")
+drink = input("What would you like to drink?")
+print("your choice of drink is:", drink)
+
+print()
+print("=========Main Dishes=========")
+print("Pancakes, Noodles, Meat Pie, Fish & Chips, Dumplings")
+dish = input("What would you like to eat?")
+print("your main dish is:", dish)
+
+print()
+print("=========Desserts=========")
+print("Ice cream, Cake, Brownies, Cookies")
+dessert = input("What would you like for desserts?")
+print("your dessert is:", dessert)
